70c4e59278ae83c01b9c83a989b8f494d003404a
Sandun-Induranga <sandutharu40@gmail.com>
2023-11-17 15:27:39 +0530

    Root Page Updated

--- lib/pages/root_page/root_page.dart
+++ lib/pages/root_page/root_page.dart
@@ -11,16 +11,26 @@ class PosApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(
-        color: Colors.white,
-        child: const Center(
-          child: Text(
-            'Mobile POS',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.deepPurple),
+              label: 'Home',
             ),
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.grey,
         ),
       ),
     );
