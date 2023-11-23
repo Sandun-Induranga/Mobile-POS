070ec7696f3ede58bcea4bf3b60d72fb23a2f885
Sandun-Induranga <sandutharu40@gmail.com>
2023-11-23 22:28:34 +0530

    Home Page Updated

--- lib/ui/pages/home_page/home_page_view.dart
+++ lib/ui/pages/home_page/home_page_view.dart
@@ -5,128 +5,153 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 40,
-      ),
+    return Container(
+      color: Colors.deepPurple[50],
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            'Dashboard',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              // color: Colors.deepPurple,
-            ),
-          ),
-          Card(
-            color: Colors.deepPurple,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 20,
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
               ),
-              child: ListTile(
-                leading: Icon(Icons.bar_chart, color: Colors.white),
-                title: Text(
-                  'Total Sales',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20,
+              color: Colors.deepPurple,
+            ),
+            alignment: Alignment.center,
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: Column(
+                children: [
+                  Text(
+                    'Manage Customers',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                subtitle: Text(
-                  '10,000',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
+                ],
               ),
             ),
           ),
-          Card(
-            color: Colors.deepPurple,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 20,
-              ),
-              child: ListTile(
-                leading: Icon(Icons.bar_chart, color: Colors.white),
-                title: Text(
-                  'Monthly Income',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20,
+          const SizedBox(height: 20),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+            child: Column(
+              children: [
+                Card(
+                  color: Colors.deepPurple,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 20,
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.bar_chart, color: Colors.white),
+                      title: Text(
+                        'Total Sales',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '10,000',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                subtitle: Text(
-                  '100,000',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
+                Card(
+                  color: Colors.deepPurpleAccent,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 20,
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.bar_chart, color: Colors.white),
+                      title: Text(
+                        'Monthly Income',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '100,000',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Card(
+                  color: Colors.deepPurple,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 20,
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.bar_chart, color: Colors.white),
+                      title: Text(
+                        'Daily Income',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '5,000',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.deepPurpleAccent,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 20,
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.bar_chart, color: Colors.white),
+                      title: Text(
+                        'Total Customers',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '100',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Card(
-            color: Colors.deepPurple,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 20,
-              ),
-              child: ListTile(
-                leading: Icon(Icons.bar_chart, color: Colors.white),
-                title: Text(
-                  'Daily Income',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20,
-                  ),
-                ),
-                subtitle: Text(
-                  '5,000',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.deepPurple,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 20,
-              ),
-              child: ListTile(
-                leading: Icon(Icons.bar_chart, color: Colors.white),
-                title: Text(
-                  'Total Customers',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20,
-                  ),
-                ),
-                subtitle: Text(
-                  '100',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          )
         ],
       ),
-    ));
+    );
   }
 }
